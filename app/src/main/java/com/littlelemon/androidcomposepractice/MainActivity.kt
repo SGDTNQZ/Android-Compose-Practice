905c85535c519c13c9089ab4eeadaef3f9777b8f
SGDTNQZ <sagadat556@gmail.com>
2023-12-08 11:41:34 +0600

    first commit

--- app/src/main/java/com/littlelemon/androidcomposepractice/MainActivity.kt
+++ app/src/main/java/com/littlelemon/androidcomposepractice/MainActivity.kt
@@ -14,11 +14,16 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -36,55 +41,71 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Column(
-                verticalArrangement = Arrangement.Center,
+            PreviewApp()
+        }
+    }
+
+    @Composable
+    fun MyApp(){
+        val label = remember { mutableStateOf("") }
+        val buttonValue7: String = "7"
+        val buttonValue8: String = "8"
+        val buttonValue9: String = "9"
+
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .border(5.dp, color = Color(R.color.black))
+                .padding(all =  10.dp)
+        ) {
+            Row (
+                horizontalArrangement = Arrangement.End,
                 modifier = Modifier
                     .fillMaxWidth()
-                    .border(
-                        5.dp,
-                        color = Color(R.color.white)
-                    )
-                    .height(50.dp)
-            )
-            {
-                Row (
-                    modifier = Modifier
-                        .height(32.dp)
-                        .border(
-                            1.dp,
-                            color = Color(R.color.purple_500)
-                        )
-                        .fillMaxWidth()
-                        .background(Color(R.color.teal_700)),
-                    horizontalArrangement = Arrangement.SpaceEvenly,
-                    verticalAlignment = Alignment.CenterVertically
-
-                ){
+                    .padding(all = 10.dp)
+            ){
+                Text(text = label.value)
+            }
+            Row (
+                horizontalArrangement = Arrangement.SpaceEvenly,
+                modifier = Modifier
+                    .fillMaxSize()
+            ){
+                Button(
+                    onClick = { label.value = "${label.value}$buttonValue7" } ,
+                    modifier = Modifier.weight(1f)
+                )
+                {
                     Text(
-                        text = stringResource(id = R.string.normal_text),
-                        color = Color(R.color.black)
-                    )
-                    Text(
-                        text = stringResource(id = R.string.bold_text),
-                        fontWeight = FontWeight.Bold,
-                        color = Color(R.color.black)
-                    )
-                    Text(
-                        text = stringResource(id = R.string.italic_text),
-                        fontStyle = FontStyle.Italic,
-                        color = Color(R.color.black)
-                    )
-                    Text(
-                        text = stringResource(id = R.string.bold_italic_text),
-                        fontStyle = FontStyle.Italic,
-                        fontWeight = FontWeight.Bold,
-                        color = Color(R.color.black)
-                    )
-
+                        text = "7")
+                }
+                Button(
+                    onClick = { label.value = "${label.value}$buttonValue8"},
+                    modifier = Modifier.weight(1f)
+                    ) {
+                    Text(text = "8")
+                }
+                Button(
+                    onClick = { label.value = "${label.value}$buttonValue9" },
+                    modifier = Modifier.weight(1f)
+                    ) {
+                    Text(text = "9")
+                }
+                Button(
+                    onClick = { label.value = "" },
+                    modifier = Modifier.weight(1f)
+                    ) {
+                    Text(text = "C")
                 }
             }
 
-
+        }
+    }
+    @Preview
+    @Composable
+    fun PreviewApp(){
+        AndroidComposePracticeTheme {
+            MyApp()
         }
     }
 }
